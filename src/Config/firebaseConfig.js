27da06052085344fc692eb6ffaec5e2b189cb908
hinakakdiya <kakdiyahina@gmail.com>
2024-08-05 14:09:06 +0530

    add

--- src/Config/firebaseConfig.js
+++ src/Config/firebaseConfig.js
@@ -1,3 +1,22 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAFIB6vbRdBvE3GR49wzIdyYmpRoumQu7o",
+//   authDomain: "firstfb-6aa63.firebaseapp.com",
+//   projectId: "firstfb-6aa63",
+//   storageBucket: "firstfb-6aa63.appspot.com",
+//   messagingSenderId: "606029072170",
+//   appId: "1:606029072170:web:5fd81038fd180ffba3c053"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -5,12 +24,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAFIB6vbRdBvE3GR49wzIdyYmpRoumQu7o",
-  authDomain: "firstfb-6aa63.firebaseapp.com",
-  projectId: "firstfb-6aa63",
-  storageBucket: "firstfb-6aa63.appspot.com",
-  messagingSenderId: "606029072170",
-  appId: "1:606029072170:web:5fd81038fd180ffba3c053"
+  apiKey: "AIzaSyD9z3hzPT9uk_1HsFF6pU3fVQGwdxnD44g",
+  authDomain: "firestore-535c6.firebaseapp.com",
+  projectId: "firestore-535c6",
+  storageBucket: "firestore-535c6.appspot.com",
+  messagingSenderId: "1084056901811",
+  appId: "1:1084056901811:web:5e6254bdd8d54bf66538c6"
 };
 
 // Initialize Firebase
--- src/app.js
+++ src/app.js
@@ -6,19 +6,14 @@ import path from "path"
 import { v4 } from "uuid"
 import cron from "node-cron"
 import { rateLimit } from "express-rate-limit"
-import { Server } from "socket.io"
 
 import admin from "firebase-admin"
 import { readFileSync } from "fs"
-import serviceAccount from "../firstfb-6aa63-firebase-adminsdk-vrfzf-452637f945.json" assert { type : "json"}
+// import serviceAccount from "../firstfb-6aa63-firebase-adminsdk-vrfzf-452637f945.json" assert { type: "json"}
+import serviceAccount from "../firestore-535c6-firebase-adminsdk-tuahb-493092faca.json" assert { type: "json"}
 
 import fileModel from "./Models/Files.js"
-import userRouter from "./Apis/userApis.js"
-import userModel from "./Models/userModel.js"
-import chatModel from "./Models/chatModel.js"
-import groupChatModel from "./Models/groupChatModel.js"
 import { timeStamp } from "console"
-import groupApi from "./Apis/groupApi.js"
 import { type } from "os"
 
 
@@ -30,16 +25,22 @@ dotenv.config()
 apiServerInstance.use(express.json())
 
 
-//<------upload file in firebase
+//upload file in firebase with postman
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    storageBucket: "firstfb-6aa63.appspot.com"
-});
 
+    //file send karva mate
+    // storageBucket: "firstfb-6aa63.appspot.com"
+
+    //database ma data add karva mate
+    storageBucket: "firestore-535c6.appspot.com"
+});
 const bucket = admin.storage().bucket()
 
+const db = admin.firestore()
 
-//<--------rateLimit----------->
+
+//rateLimit
 // const limiter = rateLimit({
 //     windowMs: 15 * 60 * 1000, // 15 minutes
 //     limit: 1,  // Limit each IP to 100 requests per `window` (here, per 15 minutes).
@@ -52,7 +53,7 @@ const bucket = admin.storage().bucket()
 // apiServerInstance.use(limiter)
 //<-------------------->
 
-//<--------cors policy---------->
+//cors policy
 apiServerInstance.use(cors({
     methods: "*",
     origin: "*",
@@ -106,10 +107,6 @@ var upload = multer({
 
 apiServerInstance.use(express.json())
 
-apiServerInstance.use(userRouter)
-
-apiServerInstance.use(groupApi)
-
 apiServerInstance.get("/server-status", (req, res) => {
     try {
         res.send("server is live")
@@ -118,6 +115,7 @@ apiServerInstance.get("/server-status", (req, res) => {
     }
 })
 
+//get image in multer
 apiServerInstance.get("/getImage/:uuid", async (req, res) => {
     console.log("12345678");
     try {
@@ -146,6 +144,7 @@ apiServerInstance.get("/getImage/:uuid", async (req, res) => {
     }
 })
 
+//upload image in multer
 apiServerInstance.post('/upload/image', upload.array("file"), async (req, res) => {
     try {
         console.log("body ==", req.body);
@@ -168,6 +167,7 @@ apiServerInstance.post('/upload/image', upload.array("file"), async (req, res) =
     }
 });
 
+//get all data in multer
 apiServerInstance.get("/getAllData", async (req, res) => {
     try {
 
@@ -180,7 +180,7 @@ apiServerInstance.get("/getAllData", async (req, res) => {
 })
 
 
-//<------upload file in firebase------->
+//upload file in firebase with postman
 apiServerInstance.post('/upload/file', upload.single('file'), async (req, res) => {
     try {
         const { file } = req
@@ -194,6 +194,56 @@ apiServerInstance.post('/upload/file', upload.single('file'), async (req, res) =
         res.status(200).json({ message: 'File uploaded successfully', downloadURL })
     } catch (error) {
         res.status(500).json({ error: error.message })
+    }
+})
+
+//upload file in firebase without postman
+const __dirname = path.resolve()
+apiServerInstance.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'http://127.0.0.1:5500/firebase.html'))
+})
+
+//<-------data add, data delete by ID and data update by ID in firestore-------->
+// add data in firebase
+apiServerInstance.post('/upload/data', async (req, res) => {
+    try {
+        const { name, email, std } = req.body;
+
+        const docRef = db.collection('users').doc();  // Create a new document reference
+        await docRef.set({
+            name,
+            email,
+            std
+        });
+
+        res.status(200).json({ message: 'User data uploaded successfully' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+// Delete user data by ID in firebase
+apiServerInstance.delete('/delete/user/:id', async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const userRef = db.collection('users').doc(userId);
+        await userRef.delete();
+        res.status(200).json({ message: 'User data deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+// Update user data by ID in firebase
+apiServerInstance.put('/update/user/:id', async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const updates= req.body
+        const userRef = db.collection('users').doc(userId);
+        await userRef.update(updates);
+        res.status(200).json({ message: 'User data update successfully' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 })
 
@@ -212,146 +262,12 @@ apiServerInstance.post('/upload/file', upload.single('file'), async (req, res) =
 //<--------------->
 
 
-//<-----create server------->
+//create server
 const severport = process.env.SERVER_PORT;
-const server = apiServerInstance.listen(severport, () => {
+apiServerInstance.listen(severport, () => {
 
     console.log(`Hello Node == ${severport}`)
 })
 //<--------------->
 
 
-//<--------socket connect with api----------->
-
-//The server which in the bracket is api server (line no. 168) and the server which is after new key word is the socker server.
-const io = new Server(server, {
-    cookie: true,
-    cors: {
-        origin: "*",
-        // origin: ["http://127.0.0.1:5500", "http://192.168.1.20:7000"] //aapde connect kariae tyare same vala no port and aapdu ipconfig
-        // origin: ["http://127.0.0.1:5501"] //koi jode connrction kariae tyare, aapdo port je go live kariae tyare aave
-    },
-});
-
-let connectedSocketUsersObj = {}
-
-// console.log(io);
-
-io.on("connection", (socket) => {
-    console.log("user connected and id is === ", socket.id);
-
-    socket.on("disconnect", (socket) => {
-        console.log("user disconnected == ", socket.id);
-    })
-
-    socket.on("register", async (doc) => {
-        console.log("register event");
-        connectedSocketUsersObj[doc.userID] = socket.id;
-        console.log(connectedSocketUsersObj);
-
-        const data = await userModel.find();
-
-
-        socket.emit("all_user_get", { data });
-
-        const chatData = await chatModel.find({ senderID: "1" });
-
-        socket.emit("chat-data", { chatData })
-
-    });
-
-    socket.on("drop_message", async (doc) => {
-        
-        console.log("drive message doc == ", doc);
-
-        const chatData = await chatModel.find({
-            $or: [
-                { senderID: doc.senderID, receiverID: doc.receiverID },
-                { senderID: doc.receiverID, receiverID: doc.senderID },
-            ],
-        });
-
-        let chatMessage
-
-        if (!chatData.length) {
-            console.log("receiverId ==", doc.receiverID);
-            const prepareChatData = new chatModel({
-                chatID: v4(),
-                senderID: doc.senderID,
-                receiverID: doc.receiverID,
-                message: doc.message
-            })
-
-
-            const savedData = await prepareChatData.save()
-            chatMessage = savedData
-        } else {
-            const prepareChatData = new chatModel({
-                chatID: chatData[0].chatID,
-                senderID: doc.senderID,
-                receiverID: doc.receiverID,
-                message: doc.message
-            })
-
-            const savedData = await prepareChatData.save()
-            chatMessage = savedData
-        }
-        console.log("chat message == ", chatMessage);
-        const user = connectedSocketUsersObj[`${doc.receiverID}`];
-        console.log("user === ", doc.receiverID, user);
-
-        socket.to(user).emit("caught_message", chatMessage);
-    });
-
-    socket.on("get_all_chat", async (doc) => {
-        const chatData = await chatModel.find({
-            $or: [
-                { senderID: doc.senderID, receiverID: doc.receiverID },
-                { senderID: doc.receiverID, receiverID: doc.senderID },
-            ],
-        });
-
-        socket.emit("catch_all_chat", { chatData })
-    })
-
-    socket.on("get_users", async (doc) => {
-        try {
-            const { search } = doc;
-            const str = `/^${search}/`;
-
-            const data = await userModel.find({
-                $or: [
-                    { name: { $regex: `^[${search}]`, $options: "i" } },
-                    { email: { $regex: `^[${search}]`, $options: "i" } },
-                ],
-            });
-
-            console.log("data === ", data);
-
-            socket.emit("searched_value", { data });
-        } catch (error) { }
-    })
-
-    // // group join
-    // socket.on("joinGroup", async (groupID) => {
-    //     socket.join(groupID)
-    //     const group = await groupChatModel.findOne({groupID})
-
-    //     socket.emit("groupMessage", group.message)
-    // })
-
-    // // group message
-    // socket.on("groupMesage", async (doc) => {
-    //     const {groupID, senderID, message} = doc
-    //     const group = await groupChatModel.findOneAndUpdate(
-    //         {groupID},
-    //         {$push : {messages: {senderID, message, timeStamp: new Date()}}},
-    //         {new: true}
-    //     )
-    //     io.to(groupID).emit("newGroupMessage", {senderID, message, timeStamp: new Date()})
-    // })
-
-    socket.on("jkl", (doc) => {
-        console.log("jkl log == ", doc);
-    });
-});
